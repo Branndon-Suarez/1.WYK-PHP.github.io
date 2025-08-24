c10a33e03d0cc0769f559be69ee3ab2d29a31c32
Branndon-Schneyder-Suarez-Jimenez <branndon255225@gmail.com>
2025-08-24 16:07:10 -0500

    Create connection.php
    Se creó el archivo de conexión a la BD MySQL con la forma de PDO

---
+++ 3.Diseño_Web-REPOSITORIO/config/Connection.php
@@ -0,0 +1,26 @@
+<?php
+
+class Connection {
+    protected $db;
+    private $dbType = 'mysql';
+    private $host = 'localhost';
+    private $dbName = 'PROYECTO_WYK';
+    private $user = 'root';
+    private $pass = '';
+
+    public function __construct() {
+        try {
+            $this->db = new PDO(
+                "{$this->dbType}:host={$this->host};dbname={$this->dbName}",
+                $this->user,
+                $this->pass
+            );
+
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->db;
+        } catch (PDOException $exception) {
+            echo "Error de conexión a la base de datos: " . $exception->getMessage();
+            exit;
+        }
+    }
+}

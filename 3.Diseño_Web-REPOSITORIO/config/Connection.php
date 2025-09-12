ada1e8c0f73a6f0c23104ba4947c645e91780616
Branndon-Schneyder-Suarez-Jimenez <branndon255225@gmail.com>
2025-09-11 19:50:57 -0500

    Update Connection.php

--- 3.Diseño_Web-REPOSITORIO/config/Connection.php
+++ 3.Diseño_Web-REPOSITORIO/config/Connection.php
@@ -4,12 +4,13 @@ use PDO;
 use PDOException;
 
 class Connection {
-    protected $db;
-    private $dbType = 'mysql';
-    private $host = 'localhost';
-    private $dbName = 'PROYECTO_WYK';
-    private $user = 'root';
-    private $pass = '';
+    private static $db;
+
+    private static $dbType = 'mysql';
+    private static $host = 'localhost';
+    private static $dbName = 'PROYECTO_WYK';
+    private static $user = 'root';
+    private static $pass = '';
 
     /* protected $db;
     private $dbType = 'mysql';
@@ -18,19 +19,33 @@ class Connection {
     private $user = 'if0_39799237';
     private $pass = 'Q2Ffn9gQHt7aZrH'; */
 
-    public function __construct() {
-        try {
-            $this->db = new PDO(
-                "{$this->dbType}:host={$this->host};dbname={$this->dbName}",
-                $this->user,
-                $this->pass
-            );
-
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->db;
-        } catch (PDOException $exception) {
-            echo "Error de conexión a la base de datos: " . $exception->getMessage();
-            exit;
+    private function __construct() {}
+ 
+    public static function getConnection() {
+        // Verifica si la conexión ya ha sido establecida
+        if (self::$db === null) {
+            try {
+                // Crea la conexión si no existe
+                self::$db = new PDO(
+                    self::$dbType . ':host=' . self::$host . ';dbname=' . self::$dbName,
+                    self::$user,
+                    self::$pass
+                );
+                
+                // Configura el modo de error para que lance excepciones
+                self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                // Establece la codificación de caracteres a UTF-8
+                self::$db->exec("SET NAMES 'utf8mb4'");
+            } catch (PDOException $e) {
+                // Si la conexión falla, registra el error y termina la ejecución
+                error_log("Error de conexión a la base de datos: " . $e->getMessage());
+                die("Error de conexión a la base de datos. Por favor, inténtelo de nuevo más tarde.");
+            }
         }
+        
+        // Devuelve la instancia de la conexión
+        return self::$db;
     }
 }
+

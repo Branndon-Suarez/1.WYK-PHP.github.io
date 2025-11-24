db47b98587b02b796fcc8590f9b3f456e1e4b4f4
Branndon-Schneyder-Suarez-Jimenez <branndon255225@gmail.com>
2025-11-23 19:17:23 -0500

    Fixed: Corección nomre APP_URL
    Debido al cambio del nombre de carpeta

--- 3.App-PHP-Nativo/config/app.php
+++ 3.App-PHP-Nativo/config/app.php
@@ -1,6 +1,6 @@
 <?php
 namespace config;
 
-const APP_URL = 'http://localhost/1.REPOSITORIO-WYK-PANADERIA.github.io/3.Diseño_Web-REPOSITORIO/';
+const APP_URL = 'http://localhost/1.WYK-PHP.github.io/3.App-PHP-Nativo/';
 /* const APP_URL = 'https://panaderia-wyk.infinityfreeapp.com/'; */
 const APP_NAME = 'WYK Panadería';
